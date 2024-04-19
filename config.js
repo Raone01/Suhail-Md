44ca8b2905e322a3adc64076356a395d9de82a2b
∆~•Sᴀɴᴀᴛᴀɴɪ Aᴅɪᴛʏᴀ•~∆ <121045125+Raone01@users.noreply.github.com>
2024-04-19 11:52:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ module.exports = {
 
 
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
+  LANG: ( process.env.THEME ||  "▪️▫️▪️ѕιℓєηт  χαямσηу🦚"  ).toUpperCase(),
 
 
 
